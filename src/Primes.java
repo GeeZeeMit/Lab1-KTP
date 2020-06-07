8cb4e28487d1e4f4f912571e4ddb058f9e12c99d
GeeZee <rekrut.makarov@mail.ru>
2020-06-07 20:16:24 +0300

    lab 1 good

---
+++ src/Primes.java
@@ -0,0 +1,21 @@
+public class Primes
+{
+    public static void main(String args[])
+    {
+        for (int i = 2; i < 100; i++)
+        {
+            if (isPrime(i)) System.out.print(i + " ");
+        }
+    }
+
+    static boolean isPrime(int n)
+    {
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
